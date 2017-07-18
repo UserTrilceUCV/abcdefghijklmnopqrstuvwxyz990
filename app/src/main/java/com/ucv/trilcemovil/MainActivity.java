65456473fce899fd20059340bd9c822657d0f4b5
EdwinCieza <ecieza.ucv@gmail.com>
2017-07-18 09:51:48 -0500

    Initial commit

---
+++ app/src/main/java/com/ucv/trilcemovil/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ucv.trilcemovil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {//hay que porobar con uno nuevo a ver mejor
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
