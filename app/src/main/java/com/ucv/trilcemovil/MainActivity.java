ddb8794120155519d10ebd3c174b82afef776ea0
UserTrilceUCV <ecieza.ucv@gmail.com>
2017-07-18 23:57:34 -0500

    b

--- app/src/main/java/com/ucv/trilcemovil/MainActivity.java
+++ app/src/main/java/com/ucv/trilcemovil/MainActivity.java
@@ -3,7 +3,7 @@ package com.ucv.trilcemovil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {//hay que porobar con uno nuevo a ver mejor
+public class MainActivity extends AppCompatActivity {//hay que porobar con uno nuevo a ver mejo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
